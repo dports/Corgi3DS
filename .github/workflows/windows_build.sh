ad3e0800a69f371975bf0e19faf49cb5c11d8f9b
DartPower <2005369+DartPower@users.noreply.github.com>
2021-04-21 19:44:28 +0300

    Update windows_build.sh

--- .github/workflows/windows_build.sh
+++ .github/workflows/windows_build.sh
@@ -1,4 +1,4 @@
 pacman -S --noconfirm mingw-w64-gmp
 mkdir /corgi3DS/build  && cd /corgi3DS/build
-x86_64-w64-mingw32-cmake ..
+x86_64-w64-mingw32-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_STATIC=ON 
 make
